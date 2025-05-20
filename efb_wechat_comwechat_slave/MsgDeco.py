c49ad1258850cce3f7ce0f5ccd6e3693e2820615
jiz4oh <me@jiz4oh.com>
2025-05-20 12:00:07 +0800

    fix: improve self-message detection in replies

--- efb_wechat_comwechat_slave/MsgDeco.py
+++ efb_wechat_comwechat_slave/MsgDeco.py
@@ -410,7 +410,8 @@ def efb_share_link_wrapper(message: dict, chat) -> Message:
             )
             prefix = ""
             master_message = False
-            if refer_svrid is not None and refer_chatusr == message["self"]:
+            from_me = (refer_chatusr or refer_fromusr) == message["self"]
+            if refer_svrid is not None and from_me:
                 try:
                     if "@chatroom" in refer_fromusr:  # 群聊中回复的消息
                         c = ChatMgr.build_efb_chat_as_group(EFBGroupChat(
@@ -423,10 +424,10 @@ def efb_share_link_wrapper(message: dict, chat) -> Message:
                     # 从 master channel 中根据微信 id 查找，如果找到说明是由 comwechat self_msg 发送过去的
                     master_message = coordinator.master.get_message_by_id(chat=c, msg_id=refer_svrid)
                 except NotImplementedError as e:
-                    pass
+                    print_exc()
             if refer_displayname is not None:
                 prefix = f"{refer_displayname}:"
-            if refer_svrid is None or (refer_chatusr == message["self"] and not master_message):
+            if refer_svrid is None or (from_me and not master_message):
                 #TODO 因为微信会将视频/文件等拆分成多条消息，refer_svrid 对应的可能是 slave_message_id 的一部分
                 #可以考虑直接将 refer_svrid 作为 target.uid，不过在回复富文本消息的时候 target.uid 是无效状态
                 if refer_msgType == 1: # 被引用的消息是文本
