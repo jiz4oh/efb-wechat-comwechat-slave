f8aa05a763a25d8771af002e3cdac10d706575f9
jiz4oh <me@jiz4oh.com>
2025-07-24 17:19:00 +0800

    feat: update alias according to at message

--- efb_wechat_comwechat_slave/ComWechat.py
+++ efb_wechat_comwechat_slave/ComWechat.py
@@ -159,6 +159,14 @@ class ComWeChatChannel(SlaveChannel):
             except:
                 name = wxid
 
+            if "<atuserlist>" in msg["extrainfo"]:
+                at_user = re.search("<atuserlist>(.*)<\/atuserlist>", msg["extrainfo"]).group(1)
+                user_list = [user for user in at_user.split(",") if user]
+                if len(user_list) == 1:
+                    alias = msg["message"].split("\u2005", 1)[0][1:-1]
+                    self.group_members[sender] = self.group_members.get(sender, {})
+                    self.group_members[sender][wxid] = alias
+
             author = ChatMgr.build_efb_chat_as_member(chat, EFBGroupMember(
                 uid = wxid,
                 name = name,
